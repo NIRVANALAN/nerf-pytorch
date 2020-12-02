067a4b8a7d8eeedd5ac81448fbc60065ece97541
yushi.lan <yushi.lan@outlook.com>
2020-12-02 21:46:07 +0800

    update

--- download_example_data.sh
+++ download_example_data.sh
@@ -1,6 +1,6 @@
-wget https://people.eecs.berkeley.edu/~bmild/nerf/tiny_nerf_data.npz
-mkdir -p data
+#wget --no-check-certificate https://people.eecs.berkeley.edu/~bmild/nerf/tiny_nerf_data.npz
+#mkdir -p data
 cd data
-wget https://people.eecs.berkeley.edu/~bmild/nerf/nerf_example_data.zip
+wget --no-check-certificate https://people.eecs.berkeley.edu/~bmild/nerf/nerf_example_data.zip
 unzip nerf_example_data.zip
 cd ..
